cfb672914632bf938288a47f40826092a0e57572
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-10 10:15:39 +0500

    first.2

---
+++ sumLibaray/src/main/java/com/example/sumlibaray/Math.kt
@@ -0,0 +1,10 @@
+package com.example.sumlibaray
+
+interface Math {
+
+    fun sum(a:Int, b:Int):Int
+    fun Sub(a:Int,b:Int):Int
+    fun multilpy(a:Int, b:Int):Int
+    fun divide(a:Int,b:Int):Int
+
+}
