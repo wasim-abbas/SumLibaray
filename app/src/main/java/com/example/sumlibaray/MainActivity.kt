6dff67da662c2f6c388629cff54d265eeb817517
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-14 15:58:53 +0500

    hello

--- app/src/main/java/com/example/sumlibaray/MainActivity.kt
+++ app/src/main/java/com/example/sumlibaray/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.sumlibaray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
