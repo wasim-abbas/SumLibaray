768b834d5e2fb6bfc5ad1ea1818115595c288f06
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-14 16:05:27 +0500

    hello

--- sumLibaray/src/main/java/com/example/sumlibaray/Add.kt
+++ sumLibaray/src/main/java/com/example/sumlibaray/Add.kt
@@ -1,13 +1,9 @@
 package com.example.sumlibaray
 
-import android.content.Context
-import android.view.View
+object Add:MyMath {
 
-object Add : MyMath {
-
-    override fun sum(a: Int, b: Int) = a + b
-    override fun Sub(a: Int, b: Int) = a - b
-    override fun multilpy(a: Int, b: Int) = a * b
-    override fun divide(a: Int, b: Int) = a / b
-    fun activity(contxt: Context) = View.inflate(contxt, R.layout.activity_sum, null)
+    override fun sum(a:Int, b:Int) =  a+b
+    override fun Sub(a: Int, b: Int) = a-b
+    override fun multilpy(a: Int, b: Int) = a*b
+    override fun divide(a: Int, b: Int) = a/b
 }
