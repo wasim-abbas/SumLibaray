fcbe606cf0e05344caac6f407260b4d048aa92f9
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-14 10:03:21 +0500

    hello

---
+++ sumLibaray/src/main/java/com/example/sumlibaray/SumActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sumlibaray
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sum)
+    }
+}
