7337413ea33f3ad107d8efdae419e3bea9016ee0
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-14 14:40:51 +0500

    hello

--- app/src/main/java/com/example/sumlibaray/MainActivity.kt
+++ app/src/main/java/com/example/sumlibaray/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sumlibaray
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
