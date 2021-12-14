11c501f63dd786af6c9eba75f1eb3eb6ce253f71
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-14 15:27:51 +0500

    hello

--- app/src/main/java/com/example/sumlibaray/MainActivity.kt
+++ app/src/main/java/com/example/sumlibaray/MainActivity.kt
@@ -1,64 +1,13 @@
 package com.example.sumlibaray
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
-    lateinit var btnAdd: Button
-    lateinit var btnSub: Button
-    lateinit var btnMultiply: Button
-    lateinit var btnDivision: Button
-    lateinit var ed1: EditText
-    lateinit var ed2: EditText
-    lateinit var textview1: TextView
-    val a:String= ""
 
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sum)
-
-
-        btnAdd = findViewById(R.id.btnAdd)
-        btnSub = findViewById(R.id.btnSub)
-        btnMultiply = findViewById(R.id.btnMultiply)
-        btnDivision = findViewById(R.id.btnDivision)
-        ed1 = findViewById(R.id.ed1)
-        ed2 = findViewById(R.id.ed2)
-        textview1 = findViewById(R.id.textview1)
-
-        btnAdd.setOnClickListener(this)
-        btnSub.setOnClickListener(this)
-        btnMultiply.setOnClickListener(this)
-        btnDivision.setOnClickListener(this)
-
-    }
-    override fun onClick(view: View) {
-        val edt1 = ed1.text.toString()
-        var edt2 = ed2.text.toString()
-
-        if (edt1 == "" || edt2 == "") {
-            ed1.error = "enter number"
-            ed2.error = "enter number"
-        } else {
-            when (view.id) {   ///add sum are from my libaray
-                R.id.btnAdd -> textview1.text = Add.sum(edt1.toInt(), edt2.toInt()).toString()
-                R.id.btnSub -> textview1.text = Add.Sub(edt1.toInt(), edt2.toInt()).toString()
-                R.id.btnMultiply -> textview1.text =
-                    Add.multilpy(edt1.toInt(), edt2.toInt()).toString()
-                R.id.btnDivision -> textview1.text =
-                    Add.divide(edt1.toInt(), edt2.toInt()).toString()
-
-            }
-            ed1.text = Editable.Factory.getInstance().newEditable(a)
-            ed2.text = Editable.Factory.getInstance().newEditable(a)
-        }
-
+        setContentView(R.layout.activity_main)
     }
 }
--- sumLibaray/src/main/java/com/example/sumlibaray/Add.kt
+++ sumLibaray/src/main/java/com/example/sumlibaray/Add.kt
@@ -3,31 +3,11 @@ package com.example.sumlibaray
 import android.content.Context
 import android.view.View
 
-object Add:MyMath {
+object Add : MyMath {
 
-    override fun sum(a:Int, b:Int):Int
-    { return  a+b
-    }
-
-    override fun Sub(a: Int, b: Int): Int {
-        return a-b
-    }
-
-    override fun multilpy(a: Int, b: Int): Int {
-
-        val mult= a*b
-        return  mult
-    }
-
-    override fun divide(a: Int, b: Int): Int {
-
-        val div = a/b
-        return div
-    }
-
-    fun activity(contxt:Context):View
-    {
-        val a = View.inflate(contxt,R.layout.activity_sum,null)
-        return a
-    }
+    override fun sum(a: Int, b: Int) = a + b
+    override fun Sub(a: Int, b: Int) = a - b
+    override fun multilpy(a: Int, b: Int) = a * b
+    override fun divide(a: Int, b: Int) = a / b
+    fun activity(contxt: Context) = View.inflate(contxt, R.layout.activity_sum, null)
 }
